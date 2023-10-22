ae1df69eb8c1955c9aebd39630652f59c992e5ba
cafaralizada1 <sd7eq683b@code.edu.az>
2023-10-22 21:47:13 +0300

    JavaScript

---
+++ index.js
@@ -0,0 +1,118 @@
+// TAPSIRIQ 1
+
+// const a = 2
+
+// const b = 8
+
+// const c = 6
+
+// const diskriminant = b * b - 4 * a * c
+
+// if (diskriminant > 0){
+
+//     const hell1 = (-b + Math.sqrt(diskriminant)) / (2 * a);
+
+//     const hell2 = (-b - Math.sqrt(diskriminant)) / (2 * a);
+
+//     console.log((hell1),(hell2) + "iki hell yolu var");
+
+// }   else if (diskriminant === 0){
+
+//     const hell = -b / (2 * a);
+
+//     console.log((hell) + "bir helli var" );
+
+// }   else {
+
+//     console.log("cavab yoxdur");
+// }
+
+
+
+
+
+// TAPSIRIQ 2
+
+// for (let i = 0; i <= 1000; i++ ){
+//     if ( i % 2 === 0){  
+//         console.log(i);
+//     }
+// }
+
+// TAPSIRIQ 3
+
+// const arr = [2, 4, 5, 9, 1];
+
+// const minimumElement = Math.min(...arr);
+
+// console.log(minimumElement);
+
+// TAPSIRIQ 4
+
+// const maximumElement = Math.max(...arr);
+
+// console.log(maximumElement);
+
+// TAPSIRIQ 5
+
+// const MinimumElement = Math.min(...arr);
+
+// const MinimumElementIndex = arr.indexOf(MinimumElement);
+
+// console.log(MinimumElement);
+
+// console.log(MinimumElementIndex);
+
+// TAPSIRIQ 6
+
+// const MaximumElement = Math.max(...arr);
+
+// const MaximumElementIndex = arr.indexOf(MaximumElement);
+
+// console.log(MaximumElement);
+
+// console.log(MaximumElementIndex);
+
+// TAPSIRIQ 7
+
+// let cem = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 2 === 1) { 
+//     cem += arr[i];
+//   }
+// }
+// console.log(cem);   //4+9=13
+
+// TAPSIRIQ 8
+
+// let i = "Men CodeAcademy Telebesiyem."
+
+// let a = i.toLowerCase();
+
+// console.log(a);
+
+// TAPSIRIQ 9
+
+// let i = "            Mən Code Academydə Programing tədrisi alıram           "
+
+// let a = i.trim();
+
+// console.log(a);
+
+// TAPSIRIQ 10
+
+// let i = "Mən Code Academy Tələbəsiyəm."
+
+// let a = i.split(" ");
+
+// console.log(a);
+
+// TAPSIRIQ 11
+
+// let i = "            Mən Code Academydə Programing tədrisi alıram            ";
+
+// let a = i.replace(/ /g, " ").length;
+
+// console.log(a.length);      //39
+
